8c1038d40d1bbcf150a3a60b7fcb29c0cddc3335
tiborpacalat <129872163+tiborpacalat@users.noreply.github.com>
2023-05-09 15:29:14 +0200

    setting up upload directory for asset testing (#2)
    * testing
    
    * disable condition to get artifacts
    
    * Update test.cy.js
    
    * cleanup

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -4,4 +4,18 @@ describe('Test DB Connection', () => {
       expect(count).to.have.lengthOf(1)
     })
   })
+
+  it('upload dir images are accessible', function () {
+    cy.loginByCSRF(
+      this.auth['admin'].username,
+      this.auth['admin'].password,
+      'survey/index/action/previewquestion/sid/841748/gid/4/qid/28'
+    )
+
+    cy.get('img').should('be.visible')
+    .and(($img) => {
+      expect($img[0].naturalWidth).to.be.greaterThan(0)
+    })
+    cy.screenshot({capture: 'runner'})
+  })
 })
