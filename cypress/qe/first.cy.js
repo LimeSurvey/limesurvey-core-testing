0837608577a21a74f13094b6c55c997de84c8772
tiborpacalat <129872163+tiborpacalat@users.noreply.github.com>
2024-03-13 12:11:07 +0100

    Update first.cy.js

--- cypress/qe/first.cy.js
+++ cypress/qe/first.cy.js
@@ -1,7 +1,12 @@
 describe('Test', () => {
-  const qeBaseUrl = `${Cypress.config('baseUrl')}:3000`.replace('/index.php/:', ':')
+  // 'http://localhost/index.php/'
+  const qeBaseUrl = `${Cypress.config('baseUrl')}`.replace(
+    '/index.php/',
+    ':3000/'
+  )
 
   it('user can access new question editor', function () {
+    cy.log(qeBaseUrl)
     cy.loginByCSRF(
       this.auth['admin'].username,
       this.auth['admin'].password,
@@ -10,8 +15,8 @@ describe('Test', () => {
 
     cy.visit(
       `${Cypress.config('baseUrl')}EditorLink?url=${encodeURIComponent(
-        `${qeBaseUrl}/#/survey/691384`
-      )}`
+        `${qeBaseUrl}#/`
+      )}&route=survey/691384`
     )
     cy.wait(3000)
     cy.get('.logo').should('be.visible')
