70f241b859436381dac82a9a869df0d154db2c28
tiborpacalat <129872163+tiborpacalat@users.noreply.github.com>
2024-03-14 14:19:54 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  video: false,
+  video: true,
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/reports',
